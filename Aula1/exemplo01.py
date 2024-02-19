f3b73a904e1dd2c9e89ceb6e5af08a6574afeb2b
Luciano Borba <luhborbafilho@gmail.com>
2024-02-19 13:11:45 -0300

    feat: Criando exemplo que conta quantidade de caracteres do nome digitado

---
+++ Aula1/exemplo01.py
@@ -0,0 +1,8 @@
+# Inserindo o Nome
+nome = input("Digite seu nome: ")
+
+# Utilizando função len para contar os caracteres
+caracteres = len(nome)
+
+# Mostrando o resultado
+print(f'O seu nome tem {caracteres} letras')
